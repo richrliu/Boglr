30e1242441a60fc4d8c5d643930e8d8977dafd84
Richard Liu <richardliu7896@gmail.com>
2015-12-08 15:32:16 -0500

    cleaned up unnecessary files

--- execute.m
+++
@@ -1,9 +0,0 @@
-clear; clc;
-
-%% Definitions
-filename = 'boggleBoard.jpg';
-
-%% Tests
-
-im = double(rgb2gray(imread(filename)));
-[subIm, corr, y1, x1] = findSubimageHighestCorr(im, windowSize);
